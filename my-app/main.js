cf1264ab1a81d0e24566c4c7382f39c381c40e4a
husseinmaad <hussein.maad@outlook.com>
2017-05-18 19:21:59 -0700

    create the react app

---
+++ my-app/main.js
@@ -0,0 +1,18 @@
+// All imports from ./helpers/functions are NOT exported using default
+import { sayHi, sayBye, instructor, instructor2, instructor3 } from './helpers/functions';
+
+// Person is exported using default, so we do not need {}.
+import Person from './helpers/default'
+
+sayHi()
+sayBye()
+
+console.log(instructor)
+console.log(instructor2)
+console.log(instructor3)
+
+const p = new Person('Elie', 'Schoppik')
+
+console.log(Person.isPerson(p))
+
+console.log(p.fullName())
